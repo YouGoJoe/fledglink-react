654e5d8f782be6fbb29eed38ed5d9fe38e683de2
Joe Staller <joestaller23@gmail.com>
2017-09-28 20:48:07 -0400

    More Episode styling

--- src/components/Episode.js
+++ src/components/Episode.js
@@ -8,29 +8,45 @@ import { colors, spacing } from '../styles';
 
 const MAX_DESCRIPTION_LENGTH = 65;
 
+const episodeContainerCss = css`
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  margin: 0 ${spacing(20)};
+  padding: ${spacing()};
+`;
+
 const EpisodeContainer = styled.div`
   ${props => props.isGrey && css`background-color: ${colors.grey};`};
 `;
 
-const CollapsedEpisode = styled.div`
-  display: flex;
-  align-items: center;
+const ExpandedEpisodeContainer = styled.div`${episodeContainerCss};`;
 
+const CollapsedEpisodeContainer = styled.div`
+  ${episodeContainerCss};
+  
   color: ${colors.teal};
   font-weight: bold;
-  padding: ${spacing()};
+  cursor: pointer;
 `;
 
 const ExpandedEpisode = styled.div`
   display: flex;
   flex-direction: column;
+  flex-grow: 1;
 `;
 
 const ExtendedButton = styled(Button)`margin: 0 ${spacing()};`;
 
 const EpisodeContentContainer = styled.span`margin: ${spacing(1.5)} 0;`;
 
-const MaterialIcon = styled.i`color: ${colors.teal};`;
+const MaterialIcon = styled.i`
+  color: ${colors.teal};
+  flex-basis: 20%;
+`;
+
+const TitleText = styled.span`flex-grow: 1;`;
 
 export default class Episode extends Component {
   constructor() {
@@ -53,30 +69,33 @@ export default class Episode extends Component {
     return (
       <EpisodeContainer {...rest}>
         {isSelected ? (
-          <ExpandedEpisode>
-            <h2>{title}</h2>
-            {isCollapsed ? (
-              <p>
-                <span>{`${description.substring(0, MAX_DESCRIPTION_LENGTH)}...`}</span>
-                <ExtendedButton clear onClick={() => this.toggleCollapsed()}>
-                  More Info
-                </ExtendedButton>
-              </p>
-            ) : (
-              <EpisodeContentContainer>
-                <p>{description}</p>
-                <Links notes={notes} />
-                <ExtendedButton clear onClick={() => this.toggleCollapsed()}>
-                  Less Info
-                </ExtendedButton>
-              </EpisodeContentContainer>
-            )}
-          </ExpandedEpisode>
-        ) : (
-          <CollapsedEpisode onClick={() => onEpisodeClick(id)}>
+          <ExpandedEpisodeContainer>
             <MaterialIcon className="material-icons">play_circle_filled</MaterialIcon>
-            <span>{title}</span>
-          </CollapsedEpisode>
+            <ExpandedEpisode>
+              <h2>{title}</h2>
+              {isCollapsed ? (
+                <p>
+                  <span>{`${description.substring(0, MAX_DESCRIPTION_LENGTH)}...`}</span>
+                  <ExtendedButton clear onClick={() => this.toggleCollapsed()}>
+                    More Info
+                  </ExtendedButton>
+                </p>
+              ) : (
+                <EpisodeContentContainer>
+                  <p>{description}</p>
+                  <Links notes={notes} />
+                  <ExtendedButton clear onClick={() => this.toggleCollapsed()}>
+                    Less Info
+                  </ExtendedButton>
+                </EpisodeContentContainer>
+              )}
+            </ExpandedEpisode>
+          </ExpandedEpisodeContainer>
+        ) : (
+          <CollapsedEpisodeContainer onClick={() => onEpisodeClick(id)}>
+            <MaterialIcon className="material-icons">play_circle_filled</MaterialIcon>
+            <TitleText>{title}</TitleText>
+          </CollapsedEpisodeContainer>
         )}
       </EpisodeContainer>
     );
